2fd28e15e199a9a1aeb75ee23ea363e67d809750
CrypticSquirrel <brejae@andrews.edu>
2020-05-26 00:23:25 -0400

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <app-navbar
+      :user="user"
+      :logout="logout"
+      ></app-navbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018 - Made with 💙 by CJ</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
